ffee82965bf3676ef8d16a2406b9570d7f4c2d22
Tomas Dulka <dulajz@gmail.com>
2017-09-12 11:36:49 +0200

    Test

--- scripts/controller/Controller.js
+++ scripts/controller/Controller.js
@@ -5,7 +5,7 @@
  class Controller {
    constructor(app, database){
      app.get('/new/*', this.input.bind(this));
-     app.get('/:code', this.find.bind(this));
+    // app.get('/:code', this.find.bind(this));
      app.get('/test', this.test.bind(this));
      this._database = database;
    }
